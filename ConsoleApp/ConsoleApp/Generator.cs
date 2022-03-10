e821372c05c93c517b817ff9fe8624a761e64d71
Alexander Pavlenko <82059804+Faust-p@users.noreply.github.com>
2022-03-10 23:36:19 +0200

    Add files via upload

---
+++ ConsoleApp/ConsoleApp/Generator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp
+{
+    internal class Generator
+    {
+        private readonly Random random = new Random();
+        private readonly int Count;
+        private int[] numbersArray;
+
+        public Generator(int count)
+        {
+            Count = count;
+            numbersArray = new int[count];
+        }
+
+        // Худший вариант, массив отсортирован по возрастанию
+        internal int[] Worst()
+        {
+            int k = 0;
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                numbersArray[i] = k;
+                k++;
+            }
+            return numbersArray;
+        }
+
+        // Средний случай, половина по возрастанию, а остальные случайные элементы (т.к. при полностью случайных значениях всегда выдаёт 0)
+        internal int[] Average()
+        {
+            for (int i = 0; i < numbersArray.Length / 2; i++)
+            {
+                int k = 0;
+                numbersArray[i] = k;
+                k++;
+            }
+            for (int i = numbersArray.Length / 2; i < numbersArray.Length; i++)
+            {
+                numbersArray[i] = random.Next(0, Count);
+            }
+            return numbersArray;
+        }
+
+        // Лучший вариант, первый элемент больше второго
+        internal int[] Best()
+        {
+            numbersArray[0] = 1; 
+            numbersArray[1] = 0;
+            for (int i = 2; i < numbersArray.Length; i++) // Далее заполнение случайными числами
+            {
+                numbersArray[i] = random.Next(1, Count);
+            }
+            return numbersArray;
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleApp
+{
+    public static class Program
+    {
+        private static HashSet<int> listsLengths = new HashSet<int> { 10000000, 20000000, 50000000, 100000000 };
+        private static Stopwatch stopwatch = new Stopwatch(); // Секундомер
+        private static double workTime;
+
+        public static void Main()
+        {
+            int[] numbersArray;
+
+            Generator generator = new Generator(1000000);
+            generator.Best(); // Холостой 
+
+            foreach (int length in listsLengths)
+            {
+                generator = new Generator(length);
+
+                numbersArray = generator.Worst(); // Худший случай
+                Stopwatch(numbersArray);
+                Console.WriteLine($"Элементов: {length}");
+                Console.WriteLine($"Худший случай - время: {workTime}");
+
+                numbersArray = generator.Average(); // Средний случай
+                Stopwatch(numbersArray);
+                Console.WriteLine($"Средний случай -  время: {workTime}");
+
+                numbersArray = generator.Best(); // Лучший случай
+                Stopwatch(numbersArray);
+                Console.WriteLine($"Лучший случай -  время: {workTime}\n");
+            }
+        }
+
+        //Проверка на возрастание
+        private static bool AscendingTest(int[] numbersArray)
+        {
+            int number = numbersArray[0];
+            for (int i = 1; i < numbersArray.Length; i++)
+            {
+                if (number > numbersArray[i])
+                {
+                    return false;
+                }
+                else
+                {
+                    number = numbersArray[i];
+                }              
+            }
+            return true;
+        }
+
+        // Секундомер
+        private static void Stopwatch(int[] numbersArray)
+        {
+            workTime = 0; // Секундомер
+            stopwatch.Reset();
+            stopwatch.Start();
+            AscendingTest(numbersArray);
+            stopwatch.Stop();
+            workTime = workTime + stopwatch.ElapsedMilliseconds;
+        }
+    }
+}
