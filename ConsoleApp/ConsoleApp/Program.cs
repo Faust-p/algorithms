de911e8cc008d31c1c52561435cddd7c94d81bdf
Alexander Pavlenko <82059804+Faust-p@users.noreply.github.com>
2022-04-07 21:02:23 +0300

    Update Program.cs

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,68 +1,70 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
+
 namespace ConsoleApp
 {
-    public static class Program
+  public static class Program
+  {
+    public static void Main()
+
     {
-        private static HashSet<int> listsLengths = new HashSet<int> { 10000000, 20000000, 50000000, 100000000 };
-        private static Stopwatch stopwatch = new Stopwatch(); // Секундомер
-        private static double workTime;
+      HashSet<int> listsLengths = new HashSet<int> { 10000000, 20000000, 50000000, 100000000 };
+      double workTime = 0;
+      int[] numbersArray;
+      Generator generator;
 
-        public static void Main()
-        {
-            int[] numbersArray;
+      foreach (int length in listsLengths)
+      {
+        generator = new Generator(length);
 
-            Generator generator = new Generator(1000000);
-            generator.Best(); // Холостой 
+        numbersArray = generator.Worst(); // Худший случай
+        workTime = Stopwatch(numbersArray);
+        Console.WriteLine("Элементов: {0}", length);
+        Console.WriteLine("Худший случай - время: {0}", workTime);
 
-            foreach (int length in listsLengths)
-            {
-                generator = new Generator(length);
 
-                numbersArray = generator.Worst(); // Худший случай
-                Stopwatch(numbersArray);
-                Console.WriteLine($"Элементов: {length}");
-                Console.WriteLine($"Худший случай - время: {workTime}");
+        numbersArray = generator.Average(); // Средний случай
+        workTime = Stopwatch(numbersArray);
+        Console.WriteLine("Средний случай - время: {0}", workTime);
 
-                numbersArray = generator.Average(); // Средний случай
-                Stopwatch(numbersArray);
-                Console.WriteLine($"Средний случай -  время: {workTime}");
 
-                numbersArray = generator.Best(); // Лучший случай
-                Stopwatch(numbersArray);
-                Console.WriteLine($"Лучший случай -  время: {workTime}\n");
-            }
-        }
-
-        //Проверка на возрастание
-        private static bool AscendingTest(int[] numbersArray)
-        {
-            int number = numbersArray[0];
-            for (int i = 1; i < numbersArray.Length; i++)
-            {
-                if (number > numbersArray[i])
-                {
-                    return false;
-                }
-                else
-                {
-                    number = numbersArray[i];
-                }              
-            }
-            return true;
-        }
-
-        // Секундомер
-        private static void Stopwatch(int[] numbersArray)
-        {
-            workTime = 0; // Секундомер
-            stopwatch.Reset();
-            stopwatch.Start();
-            AscendingTest(numbersArray);
-            stopwatch.Stop();
-            workTime = workTime + stopwatch.ElapsedMilliseconds;
-        }
+        numbersArray = generator.Best(); // Лучший случай
+        workTime = Stopwatch(numbersArray);
+        Console.WriteLine("Лучший случай - время: {0}\n", workTime);
+      }
+      Console.ReadKey();
     }
+
+
+    //Проверка на возрастание
+    private static bool AscendingTest(int[] numbersArray)
+    {
+      for (int i = 1; i < numbersArray.Length - 1; i++)
+      {
+        if (numbersArray[i] > numbersArray[i + 1])
+        {
+          return false;
+        }     
+      }
+      return true;
+    }
+
+
+    // Секундомер
+    private static double Stopwatch(int[] numbersArray)
+    {
+      Stopwatch stopwatch = new Stopwatch(); // Секундомер
+      double workTime = 0;
+      AscendingTest(numbersArray);
+      workTime = 0; // Секундомер
+      stopwatch.Reset();
+      stopwatch.Start();
+      AscendingTest(numbersArray);
+      stopwatch.Stop();
+      workTime = workTime + stopwatch.ElapsedMilliseconds;
+      return workTime;
+    }    
+  }
 }
