2760596900489d1d28a83240aeb989ffd35211ff
Alexander Pavlenko <82059804+Faust-p@users.noreply.github.com>
2022-06-02 18:04:49 +0300

    Add files via upload

---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace lab7
+{
+    public class Program
+    {
+        private static List<int> numbersCountList = new List<int> { 1000, 2000, 3000, 4000, 5000 };
+        private delegate int[] Sorting(int[] arr);
+        private static Random rand = new Random();
+        private static int[] UnsortedArr(int length)
+        {
+            int[] arr = new int[length];
+
+            for (var i = 0; i < arr.Length; i++)
+                arr[i] = rand.Next(1, length);
+
+            return arr;
+        }
+
+        private static void Idle()
+        {
+            int[] arr = new int[10000];
+
+            for (var i = 0; i < arr.Length; i++)
+                arr[i] = rand.Next(1, 1000);
+
+            Sort.Quick(arr);
+        }
+        private static double Stopwatch(int[] arr, Sorting sort)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            sort(arr);
+            sw.Stop();
+
+            return sw.ElapsedMilliseconds;
+        }
+        static void Main(string[] args)
+        {
+            Idle();
+
+            foreach (var length in numbersCountList)
+            {
+                int[] unsortedMas = UnsortedArr(length);
+                int[] arr = new int[length];
+
+                Console.WriteLine($"- - - {length} ЭЛЕМЕНТОВ - - -");
+
+                Array.Copy(unsortedMas, arr, length);
+                double timeWork = Stopwatch(arr, Sort.Quick);
+                Console.WriteLine("{0,-16}{1,-8}", "Быстрая: ", timeWork);
+
+                Array.Copy(unsortedMas, arr, length);
+                timeWork = Stopwatch(arr, Sort.Merge);
+                Console.WriteLine("{0,-16}{1,-8}", "Слиянием: ", timeWork);
+
+                Array.Copy(unsortedMas, arr, length);
+                timeWork = Stopwatch(arr, Sort.Heap);
+                Console.WriteLine("{0,-16}{1,-8}", "Пирамидальная: ", timeWork);
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ lab7/lab7/Sort.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab7
+{
+    public static class Sort
+    {
+        private static void Swap(int[] arr, int i, int j)
+        {
+            int swap = arr[i];
+            arr[i] = arr[j];
+            arr[j] = swap;
+        }
+
+        // Быстрая сортировка
+        public static int[] Quick(int[] arr)
+        {
+            return Quick(arr, 0, arr.Length - 1);
+        }
+
+        private static int[] Quick(int[] arr, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return arr;
+            }
+            int index = ReturnIndex(arr, minIndex, maxIndex);
+            Quick(arr, minIndex, index - 1);
+            Quick(arr, index + 1, maxIndex);
+            return arr;
+        }
+
+        private static int ReturnIndex(int[] arr, int minIndex, int maxIndex)
+        {
+            int marker = minIndex, index = minIndex;
+            while (index <= maxIndex)
+            {
+                if (arr[index] <= arr[maxIndex])
+                {
+                    Swap(arr, marker, index);
+                    marker++;
+                }
+                index++;
+            }
+            return marker - 1;
+        }
+
+        // Сортировка слиянием
+        public static int[] Merge(int[] arr)
+        {
+            return Merge(arr, 0, arr.Length - 1);
+        }
+        private static int[] Merge(int[] arr, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                int midIndex = (lowIndex + highIndex) / 2;
+                Merge(arr, lowIndex, midIndex);
+                Merge(arr, midIndex + 1, highIndex);
+                if (arr[midIndex] > arr[midIndex + 1])
+                {
+                    Copy(arr, lowIndex, midIndex, highIndex);
+                }
+            }
+            return arr;
+        }
+        private static void Copy(int[] arr, int lowIndex, int midIndex, int highIndex)
+        {
+            int indexLeft = lowIndex, indexRight = midIndex + 1, k = 0;
+            int[] tempArr = new int[highIndex - lowIndex + 1];
+            while ((indexLeft <= midIndex) && (indexRight <= highIndex))
+            {
+                if (arr[indexLeft] < arr[indexRight])
+                {
+                    tempArr[k++] = arr[indexLeft++];
+                }
+                else
+                {
+                    tempArr[k++] = arr[indexRight++];
+                }
+            }
+            while (indexRight <= highIndex)
+            {
+                tempArr[k++] = arr[indexRight++];
+            }
+            while (indexLeft <= midIndex)
+            {
+                tempArr[k++] = arr[indexLeft++];
+            }
+            for (indexLeft = lowIndex, k = 0; indexLeft <= highIndex; indexLeft++, k++)
+            {
+                arr[indexLeft] = tempArr[k];
+            }             
+        }
+
+        // Пирамидальная сортировка
+        public static int[] Heap(int[] arr)
+        {
+            return Heap(arr, arr.Length);
+        }
+        private static int[] Heap(int[] arr, int lenght)
+        {
+            lenght = arr.Length - 1;
+            int index = lenght / 2;
+            while (index >= 0)
+            {
+                Heapify(arr, lenght, index);
+                index--;
+            }
+            index = lenght;
+            while (index >= 0)
+            {
+                Swap(arr, 0, index);
+                Heapify(arr, index, 0);
+                index--;
+            }
+            return arr;
+        }
+        private static void Heapify(int[] arr, int lenght, int index)
+        {
+            int largest = index,
+            minIndex = 2 * index + 1,
+            maxIndex = 2 * index + 2;
+            if (minIndex < lenght)
+            {
+                if (arr[minIndex] > arr[maxIndex])
+                {
+                    largest = minIndex;
+                }
+                else
+                {
+                    largest = maxIndex;
+                }
+            }
+            if (largest != index)
+            {
+                Swap(arr, index, largest);
+                Heapify(arr, lenght, largest);
+            }
+        }
+    }
+}
