ffb49010733d7a53393cadb56dd456a6d0981ddd
Alexander Pavlenko <82059804+Faust-p@users.noreply.github.com>
2022-06-02 18:06:05 +0300

    Add files via upload

---
+++ lab8/lab8/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace lab8
+{
+    class Program
+    {
+        private static List<int> numbersCountList = new List<int> { 1000, 2000, 3000, 4000, 5000 };
+        private static Random rand = new Random();
+        private delegate int[] sort(int[] arr);
+
+        private static void Idle()
+        {
+            int[] arr = new int[10000];
+
+            for (var i = 0; i < arr.Length; i++)
+                arr[i] = rand.Next(1, 1000);
+
+            Sort.Counting(arr);
+        }
+
+        private static double Stopwatch(int[] arr, sort sort)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            sort(arr);
+            sw.Stop();
+
+            return sw.ElapsedMilliseconds;
+        }
+
+        private static int[] UnsortedMas(int length)
+        {
+            int[] arr = new int[length];
+
+            for (var i = 0; i < arr.Length; i++)
+                arr[i] = rand.Next(100, 1000);
+
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            Idle();
+            foreach (var length in numbersCountList)
+            {
+                int[] unsortedMas = UnsortedMas(length);
+                int[] arr = new int[length];
+
+                Console.WriteLine($"- - - {length} ЭЛЕМЕНТОВ - - -");
+
+                Array.Copy(unsortedMas, arr, length);
+                double timeWork = Stopwatch(arr, Sort.Counting);
+                Console.WriteLine("{0,-16}{1,-8}", "Подсчетом: ", timeWork);
+
+                Array.Copy(unsortedMas, arr, length);
+                timeWork = Stopwatch(arr, Sort.Pigeonhole);
+                Console.WriteLine("{0,-16}{1,-8}", "Голубиная: ", timeWork);
+
+                Array.Copy(unsortedMas, arr, length);
+                timeWork = Stopwatch(arr, Sort.Bucket);
+                Console.WriteLine("{0,-16}{1,-8}", "Блочная: ", timeWork);
+
+                Array.Copy(unsortedMas, arr, length);
+                timeWork = Stopwatch(arr, Sort.Radix);
+                Console.WriteLine("{0,-16}{1,-8}", "Поразрядная: ", timeWork);
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab8/lab8/Sort.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace lab8
+{
+    public static class Sort
+    {
+        // Сортировка подсчетом
+        public static int[] Counting(int[] arr)
+        {
+            return Counting(arr, arr.Min(), arr.Max());
+        }
+
+        private static int[] Counting(int[] arr, int min, int max)
+        {
+            int[] count = new int[max - min + 1];
+            int k = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                count[arr[i] - min]++;
+            }
+            for (int i = min; i <= max; i++)
+            {
+                while (count[i - min]-- > 0)
+                {
+                    arr[k++] = i;
+                }
+            }
+            return arr;
+        }
+
+        // Голубиная сортировка
+        public static int[] Pigeonhole(int[] arr)
+        {
+            int k = 0;
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dictionary.ContainsKey(arr[i]))
+                {
+                    dictionary[arr[i]]++;
+                }
+                else
+                {
+                    dictionary.Add(arr[i], 1);
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dictionary.ContainsKey(i))
+                {
+                    for (int j = 0; j < dictionary[i]; j++)
+                    {
+                        arr[k++] = i;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        // Блочная сортировка
+        public static int[] Bucket(int[] arr)
+        {
+            int min = arr.Min(), max = arr.Max(), count = 0;
+            List<int>[] bucket = new List<int>[max - min + 1];
+
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                bucket[i] = new List<int>();
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bucket[arr[i] - min].Add(arr[i]);
+            }
+
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                for (int j = 0; j < bucket[i].Count; j++)
+                {
+                    arr[count++] = bucket[i][j];
+
+                }
+            }
+            return arr;
+        }
+
+        // Поразрядная сортировка
+        public static int[] Radix(int[] arr)
+        {
+            return Radix(arr, 10, (int)Math.Log10(arr.Max()) + 1);
+        }
+
+        private static int[] Radix(int[] arr, int range, int lengthNumber)
+        {
+            List<int>[] bucket = new List<int>[range];
+
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                bucket[i] = new List<int>();
+            }
+
+            for (int i = 0; i < lengthNumber; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    bucket[(arr[j] % (int)Math.Pow(range, i + 1)) / (int)Math.Pow(range, i)].Add(arr[j]);
+
+                }
+                int count = 0;
+                for (int j = 0; j < bucket.Length; j++)
+                {
+                    for (int k = 0; k < bucket[j].Count; k++)
+                    {
+                        arr[count++] = bucket[j][k];
+
+                    }
+                }
+                for (int k = 0; k < bucket.Length; k++)
+                {
+                    bucket[k].Clear();
+
+                }
+            }
+            return arr;
+        }
+    }
+}
